66cbdce1bd4dc0168e8bfebe0e3c3ebd639ebe65
Omar Zunic <omznc@pm.me>
2023-12-02 14:18:22 +0100

    Day 2

--- day-02/target/debug/deps/part1.d
+++
@@ -1,6 +0,0 @@
-C:\Users\omznc\RustroverProjects\aoc-23\day-02\target\debug\deps\part1.exe: src\bin\part1.rs src\bin\../../input.txt
-
-C:\Users\omznc\RustroverProjects\aoc-23\day-02\target\debug\deps\part1.d: src\bin\part1.rs src\bin\../../input.txt
-
-src\bin\part1.rs:
-src\bin\../../input.txt:
--- day-02/target/debug/part1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\omznc\RustroverProjects\aoc-23\day-02\target\debug\part1.exe: C:\Users\omznc\RustroverProjects\aoc-23\day-02\input.txt C:\Users\omznc\RustroverProjects\aoc-23\day-02\src\bin\part1.rs
